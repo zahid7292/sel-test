d4c1565e6dcb27b6d50589645e0ceb9839cac4d6
zahidraza <md-zahid.raza@outlook.com>
2019-04-01 08:33:07 +0530

    Update Category.jsx

--- src/js/pages/Category.jsx
+++ src/js/pages/Category.jsx
@@ -1,60 +1,10 @@
 import React from "react";
-
 import { withStyles } from "@material-ui/core/styles";
-
-import {
-  Create,
-  Edit,
-  List,
-  SimpleForm,
-  TextInput,
-  TextField,
-  required,
-  minLength,
-  Datagrid,
-  EditButton,
-} from "jazasoft";
-
-const layout = "modern";
-
-const createStyle = theme => ({});
-
-export const CreateCategory = withStyles(createStyle)(
-  ({ classes, ...props }) => {
-    return (
-      <Create layout={layout} {...props}>
-        <SimpleForm className={classes.form} redirect="home">
-          <TextInput source="name" validate={[required(), minLength(2)]} />
-          <TextInput source="description" />
-        </SimpleForm>
-      </Create>
-    );
-  }
-);
-
-const editStyle = theme => ({});
-
-export const EditCategory = withStyles(editStyle)(({ classes, ...props }) => {
-  return (
-    <Edit layout={layout} {...props}>
-      <SimpleForm
-        className={classes.form}
-        redirect="home"
-        form="record-form-edit"
-      >
-        <TextInput source="name" validate={[required(), minLength(2)]} />
-        <TextInput source="description" />
-      </SimpleForm>
-    </Edit>
-  );
-});
+import { List, TextField, Datagrid, EditButton } from "jazasoft";
 
 const homeStyle = theme => ({
   buttonEdit: {
     width: theme.spacing.unit * 14
-  },
-  buttonDelete: {
-    width: theme.spacing.unit * 16
   },
 });
 
@@ -65,7 +15,6 @@ export const CategoryModernHome = withStyles(homeStyle)(
         <Datagrid>
           <TextField source="name" />
           <EditButton cellClassName={classes.buttonEdit} />
-          {/* <DeleteButton cellClassName={classes.buttonDelete} /> */}
         </Datagrid>
       </List>
     );
